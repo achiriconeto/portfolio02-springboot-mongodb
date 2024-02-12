f71f300df95f635a91e17b9582bc93959db61bcd
achiriconeto <achiriconeto.jobs@gmail.com>
2024-02-12 13:10:25 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/vortexit/mongo/Portfolio02Application.java
@@ -0,0 +1,13 @@
+package com.vortexit.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vortexit/mongo/Portfolio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexit.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
